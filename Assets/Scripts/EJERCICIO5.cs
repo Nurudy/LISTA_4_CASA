8b12a302f2738d334750f045ae7e3d7be9035bcb
Nurudy <nurudilart@gmail.com>
2022-12-14 23:30:51 +0100

    ehe

--- Assets/Scripts/EJERCICIO5.cs
+++ Assets/Scripts/EJERCICIO5.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class EJERCICIO5 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float Y;
+    public float Z;
+    public float X;
+
+    private bool LimitBounds(float LIMX, float LIMY, float LIMZ)//los absolutos que habrán sobre los valores
     {
-        
+        if (transform.position.x <= LIMX && transform.position.y <= LIMY && transform.position.z <= LIMZ)
+        {
+            return true;//estando entre sus tres limites de variables, valdrá true.
+        }
+        if (transform.position.x >= (-1 * LIMX) && transform.position.y >= (-1 * LIMY) && transform.position.z >= (-1 * LIMZ))
+        {
+            return true;//estando entre sus tres limites, valdrá true
+        }
+        else
+        {
+            return false;//si no pertenece a estos limites, será false
+        }
+
+
+
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        LimitBounds(X, Y, Z);
+
+            if (LimitBounds(X, Y, Z) == true)
+        {
+            Debug.Log(message: $"The object is not out of limits");
+        }
+        else
+        {
+            Debug.Log(message: $"The object is out of limits");
+        }
     }
 }
+        
